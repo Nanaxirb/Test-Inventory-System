fb90c9d14a9c17181dad08759aa6f49a4cb07e5d
Nanaxirb <funtomhive@gmail.com>
2018-03-15 23:30:47 -0400

    Inventory
    Inventory UI- Done
    Converting JSON into Object- In Progress

---
+++ Json Inventory System/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour {
+
+    string path;
+    string jsonString;
+
+	void Start () {
+        path = Application.streamingAssetsPath + "/Item.json";
+        jsonString = File.ReadAllText(path);
+
+	}
+	
+
+}
+
+[System.Serializable]
+public class Item {
+    public int Id;
+    public string Title;
+    public int Value;
+
+    public Item(int id, string title, int value) {
+        this.Id = id;
+        this.Title = title;
+        this.Value = value;
+    }
+}
+
